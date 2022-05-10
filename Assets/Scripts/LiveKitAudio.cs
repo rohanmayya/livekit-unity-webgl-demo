7ef1c4950c620e8e771ab238e1eb309f4d2fbeb2
Rohan Mayya <rohan.mayya@gmail.com>
2022-05-10 17:20:16 +0530

    add latest livekit version

--- Assets/Scripts/LiveKitAudio.cs
+++ Assets/Scripts/LiveKitAudio.cs
@@ -3,8 +3,15 @@ using System.Collections.Generic;
 using Mirror;
 using UnityEngine;
 
-public class LiveKitAudio : MonoBehaviour
+public struct LiveKitAuth
 {
+    public string token;
+}
+
+public class LiveKitAudio : MonoBehaviour
+{   
+     // room;
+    Setup setup;
     public SphereCollider sphereCollider;
 }
 
